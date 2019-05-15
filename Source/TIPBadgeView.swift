379a121a5827bc8173d8befa13046decc43ffd1a
iscoolapp <software@iscoolapp.net>
2019-05-15 08:30:04 -0300

    swift 5
    swift 5

--- Source/TIPBadgeView.swift
+++ Source/TIPBadgeView.swift
@@ -8,72 +8,72 @@
 
 
 public class TIPBadgeView: UIView {
-    
-    public var label: UILabel!
-    
-    var topLabelConstraint:NSLayoutConstraint?
-    var bottomLabelConstraint:NSLayoutConstraint?
-    public var leftLabelConstraint:NSLayoutConstraint?
-    public var rightLabelConstraint:NSLayoutConstraint?
-    
-    public let STANDARD_MARGIN : CGFloat = 2
-    public let LARGER_MARGIN : CGFloat = 3
-    
-    public init(){
-        super.init(frame: CGRect(x: 0, y: 0, width: 18, height: 18))
-        self.backgroundColor = UIColor.red
-        self.layer.zPosition = 1000
-        self.layer.cornerRadius = 9
-        self.layer.masksToBounds = true
-        addLabel()
+
+  public var label: UILabel!
+
+  var topLabelConstraint: NSLayoutConstraint?
+  var bottomLabelConstraint: NSLayoutConstraint?
+  public var leftLabelConstraint: NSLayoutConstraint?
+  public var rightLabelConstraint: NSLayoutConstraint?
+
+  public let STANDARD_MARGIN: CGFloat = 2
+  public let LARGER_MARGIN: CGFloat = 3
+
+  public init() {
+    super.init(frame: CGRect(x: 0, y: 0, width: 18, height: 18))
+    self.backgroundColor = UIColor.red
+    self.layer.zPosition = 1000
+    self.layer.cornerRadius = 9
+    self.layer.masksToBounds = true
+    addLabel()
+  }
+
+  public required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func addLabel() {
+    label = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.backgroundColor = UIColor.clear
+    label.textColor = UIColor.white
+    label.textAlignment = NSTextAlignment.left
+    label.font = UIFont(name: self.label.font.fontName, size: 12.0)
+    label.layer.cornerRadius = 9
+    label.layer.masksToBounds = true
+
+    self.addSubview(label)
+    addLabelConstraints()
+  }
+
+  func addLabelConstraints() {
+    topLabelConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self.label!, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1.0, constant: 0)
+
+    bottomLabelConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self.label!, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1.0, constant: 0)
+
+    leftLabelConstraint = NSLayoutConstraint(item: self.label, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1.0, constant: 2.0)
+    leftLabelConstraint!.priority = UILayoutPriority(rawValue: 1000.0)
+
+    rightLabelConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutConstraint.Attribute.right, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self.label, attribute: NSLayoutConstraint.Attribute.right, multiplier: 1.0, constant: 2.0)
+    rightLabelConstraint!.priority = UILayoutPriority(rawValue: 1000.0)
+
+    self.addConstraints([topLabelConstraint!, bottomLabelConstraint!, leftLabelConstraint!, rightLabelConstraint!])
+  }
+
+  public func setBadgeValue(_ val: Int) {
+    label.text = " \(val) "
+    setMarginsForVal(val)
+  }
+
+  public func setMarginsForVal(_ val: Int) {
+    if val < 10 {
+      leftLabelConstraint!.constant = LARGER_MARGIN
+      rightLabelConstraint!.constant = LARGER_MARGIN
+    } else {
+      leftLabelConstraint!.constant = STANDARD_MARGIN
+      rightLabelConstraint!.constant = STANDARD_MARGIN
     }
-    
-    public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func addLabel(){
-        label = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = UIColor.clear
-        label.textColor = UIColor.white
-        label.textAlignment = NSTextAlignment.left
-        label.font = UIFont(name: self.label.font.fontName, size: 12.0)
-        label.layer.cornerRadius = 9
-        label.layer.masksToBounds = true
-        
-        self.addSubview(label)
-        addLabelConstraints()
-    }
-    
-    func addLabelConstraints(){
-        topLabelConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.label!, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 0)
-        
-        bottomLabelConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: self.label!, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 0)
-        
-        leftLabelConstraint = NSLayoutConstraint(item: self.label, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 2.0)
-        leftLabelConstraint!.priority = UILayoutPriority(rawValue: 1000.0)
-        
-        rightLabelConstraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: self.label, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: 2.0)
-        rightLabelConstraint!.priority = UILayoutPriority(rawValue: 1000.0)
-        
-        self.addConstraints([topLabelConstraint!, bottomLabelConstraint!, leftLabelConstraint!, rightLabelConstraint!])
-    }
-    
-    public func setBadgeValue(_ val : Int){
-            label.text = " \(val) "
-            setMarginsForVal(val)
-    }
-    
-    public func setMarginsForVal(_ val : Int){
-        if val < 10 {
-            leftLabelConstraint!.constant = LARGER_MARGIN
-            rightLabelConstraint!.constant = LARGER_MARGIN
-        } else {
-            leftLabelConstraint!.constant = STANDARD_MARGIN
-            rightLabelConstraint!.constant = STANDARD_MARGIN
-        }
-        self.layoutIfNeeded()
-    }
-    
+    self.layoutIfNeeded()
+  }
+
 }
