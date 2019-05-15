932864f69cc91487403781c50f8f30dd22011e38
iscoolapp <software@iscoolapp.net>
2019-05-15 08:30:52 -0300

    swift 5
    swift 5

--- Source/TIPBadgeObject.swift
+++ Source/TIPBadgeObject.swift
@@ -9,86 +9,86 @@
 import Foundation
 
 public protocol TIPBadgeObject {
-    var badgeValue: Int {get set}
+  var badgeValue: Int { get set }
 }
 
 
-public class TIPViewObject: NSObject, TIPBadgeObject{
-    
-    public var observerTriggered: Bool = false
-    public var badgeValue: Int = 0 {
-        willSet(newVal){
-            changeBadgeValue(newVal)
-        }
+public class TIPViewObject: NSObject, TIPBadgeObject {
+
+  public var observerTriggered: Bool = false
+  public var badgeValue: Int = 0 {
+    willSet(newVal) {
+      changeBadgeValue(newVal)
     }
-    
-    public weak var view: UIView?
-    public weak var badgeView:TIPBadgeView?
-    
-    public init(view: UIView){
-        self.view = view
-        super.init()
+  }
+
+  public weak var view: UIView?
+  public weak var badgeView: TIPBadgeView?
+
+  public init(view: UIView) {
+    self.view = view
+    super.init()
+  }
+
+  func addBadge() {
+    let bv: TIPBadgeView? = TIPBadgeView()
+    self.view!.addSubview(bv!)
+
+    bv!.translatesAutoresizingMaskIntoConstraints = false
+
+    let badgeHeightConstraint = NSLayoutConstraint(item: bv!, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.height, multiplier: 1.0, constant: 18.0)
+
+    bv!.addConstraints([badgeHeightConstraint])
+
+    let rightConstraint = NSLayoutConstraint(item: self.view!, attribute: NSLayoutConstraint.Attribute.right, relatedBy: NSLayoutConstraint.Relation.equal, toItem: bv!, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1.0, constant: 7.0)
+
+    let topConstraint = NSLayoutConstraint(item: self.view!, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: bv!, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1.0, constant: 5.0)
+
+    self.view!.addConstraints([rightConstraint, topConstraint])
+
+    self.badgeView = bv
+  }
+
+  public func changeBadgeValue(_ value: Int) {
+    if value > 0 {
+      if self.badgeView == nil {
+        addBadge()
+      }
+      self.badgeView!.setBadgeValue(value)
+    } else {
+      clearBadge()
     }
-    
-    func addBadge(){
-       let bv : TIPBadgeView? = TIPBadgeView()
-       self.view!.addSubview(bv!)
-        
-       bv!.translatesAutoresizingMaskIntoConstraints = false
-        
-        let badgeHeightConstraint = NSLayoutConstraint(item: bv!, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1.0, constant: 18.0)
-        
-        bv!.addConstraints([badgeHeightConstraint])
-        
-        let rightConstraint = NSLayoutConstraint(item: self.view!, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: bv!, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 7.0)
-        
-        let topConstraint = NSLayoutConstraint(item: self.view!, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: bv!, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 5.0)
-        
-        self.view!.addConstraints([rightConstraint, topConstraint])
-        
-        self.badgeView = bv
+  }
+
+  public func clearBadge() {
+    if self.badgeView != nil {
+      self.badgeView!.removeFromSuperview()
+      self.badgeView = nil
     }
-    
-    public func changeBadgeValue(_ value : Int){
-        if value > 0 {
-            if self.badgeView == nil {
-                addBadge()
-            }
-            self.badgeView!.setBadgeValue(value)
-        } else {
-            clearBadge()
-        }
-    }
-    
-    public func clearBadge(){
-        if self.badgeView != nil {
-            self.badgeView!.removeFromSuperview()
-            self.badgeView = nil
-        }
-    }
-    
+  }
+
 }
 
 public class TIPTabBarItemObject: NSObject, TIPBadgeObject {
-    
-    public weak var tabBar:UITabBarItem?
-    
-    public var badgeValue: Int = 0 {
-        willSet(newVal){
-            changeBadgeValue(newVal)
-        }
+
+  public weak var tabBar: UITabBarItem?
+
+  public var badgeValue: Int = 0 {
+    willSet(newVal) {
+      changeBadgeValue(newVal)
     }
-    
-    public init(tabBar: UITabBarItem){
-        self.tabBar = tabBar
-        super.init()
+  }
+
+  public init(tabBar: UITabBarItem) {
+    self.tabBar = tabBar
+    super.init()
+  }
+
+  public func changeBadgeValue(_ value: Int) {
+    if value > 0 {
+      self.tabBar!.badgeValue = "\(value)"
+    } else {
+      self.tabBar!.badgeValue = nil
     }
-    
-    public func changeBadgeValue(_ value : Int){
-        if value > 0 {
-            self.tabBar!.badgeValue = "\(value)"
-        } else {
-            self.tabBar!.badgeValue = nil
-        }
-    }
+  }
 }
